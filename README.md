a47371db3b6df8bf988fe8f5bb243658942e2f41
ryn1x <ryn1x@tutanota.com>
2018-12-27 12:55:44 -0700

    update documentation

--- README.md
+++ README.md
@@ -7,48 +7,33 @@ DEPENDANCIES
 ============
 
 ## Unix
-```
-git clone https://github.com/ryn1x/TinyTIFF.git
-cd TinyTIFF
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-```
+
+- `git clone https://github.com/ryn1x/TinyTIFF.git`
+- `cd TinyTIFF`
+- `mkdir build`
+- `cd build`
+- `cmake ..`
+- `make`
+- `sudo make install`
+
 
 ## Windows
-```
-git clone https://github.com/ryn1x/TinyTIFF.git
-cd TinyTIFF
-mkdir build
-cd build
-cmake ..
-cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -G "Visual Studio 15 2017 win64" ..
-build generated ".sln" file with visual studio
-```
 
-SYNOPSIS
-========
+- `git clone https://github.com/ryn1x/TinyTIFF.git`
+- `cd TinyTIFF`
+- `mkdir build`
+- `cd build`
+- `cmake ..`
+- `cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -G "Visual Studio 15 2017 win64" ..`
+- build generated ".sln" file with visual studio
 
-```
-// read
-let tiff = reader_open("./tests/test_data/cell32.tif").unwrap();
-let bits = reader_bits_per_sample(tiff, 0);
-let width = reader_width(tiff);
-let height = reader_height(tiff);
-let size = (width * height) as usize;
-let mut buffer: Vec<f32> = vec![0f32; size];
-reader_sample_data(tiff, &buffer, 0);
-reader_close(tiff);
+DOCUMENTATION
+============
+`cargo doc --open`
 
-// manuipulate
-for px in &mut buffer {
-    *px += 42f32;
-}
+COPYRIGHT AND LICENSE
+=====================
 
-// write
-let tiff = writer_open("./tests/test_data/cell32_mod.tif", bits, width, height).unwrap();
-writer_write_image_float(tiff, &buffer);
-writer_close(tiff, "test description");
+Copyright 2018 ryn1x
 
+This library is free software; you can redistribute it and/or modify it under the MIT or APACHE-2.0 licenses.
--- src/lib.rs
+++ src/lib.rs
@@ -7,50 +7,55 @@
 //! ============
 //! 
 //! ## Unix
-//! ```
-//! git clone https://github.com/ryn1x/TinyTIFF.git
-//! cd TinyTIFF
-//! mkdir build
-//! cd build
-//! cmake ..
-//! make
-//! sudo make install
-//! ```
+//! 
+//! - `git clone https://github.com/ryn1x/TinyTIFF.git`
+//! - `cd TinyTIFF`
+//! - `mkdir build`
+//! - `cd build`
+//! - `cmake ..`
+//! - `make`
+//! - `sudo make install`
+//! 
 //! 
 //! ## Windows
-//! ```
-//! git clone https://github.com/ryn1x/TinyTIFF.git
-//! cd TinyTIFF
-//! mkdir build
-//! cd build
-//! cmake ..
-//! cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -G "Visual Studio 15 2017 win64" ..
-//! build generated ".sln" file with visual studio
-//! ```
+//! 
+//! - `git clone https://github.com/ryn1x/TinyTIFF.git`
+//! - `cd TinyTIFF`
+//! - `mkdir build`
+//! - `cd build`
+//! - `cmake ..`
+//! - `cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -G "Visual Studio 15 2017 win64" ..`
+//! - build generated ".sln" file with visual studio
 //! 
 //! SYNOPSIS
 //! ========
 //!
 //! ```
-//! // read
-//! let tiff = reader_open("./tests/test_data/cell32.tif").unwrap();
-//! let bits = reader_bits_per_sample(tiff, 0);
-//! let width = reader_width(tiff);
-//! let height = reader_height(tiff);
-//! let size = (width * height) as usize;
-//! let mut buffer: Vec<f32> = vec![0f32; size];
-//! reader_sample_data(tiff, &buffer, 0);
-//! reader_close(tiff);
-//!
-//! // manuipulate
-//! for px in &mut buffer {
-//!     *px += 42f32;
+//! extern crate tiny_tiff;
+//! 
+//! use tiny_tiff::*;
+//! 
+//! fn manipulate_image() {
+//!     // read
+//!     let tiff = reader_open("./tests/test_data/cell32.tif").unwrap();
+//!     let bits = reader_bits_per_sample(tiff, 0);
+//!     let width = reader_width(tiff);
+//!     let height = reader_height(tiff);
+//!     let size = (width * height) as usize;
+//!     let mut buffer: Vec<f32> = vec![0f32; size];
+//!     reader_sample_data(tiff, &buffer, 0);
+//!     reader_close(tiff);
+//! 
+//!     // manipulate
+//!     for px in &mut buffer {
+//!         *px += 42f32;
+//!     }
+//! 
+//!     // write
+//!     let tiff = writer_open("./tests/test_data/cell32_mod.tif", bits, width, height).unwrap();
+//!     writer_write_image_float(tiff, &buffer);
+//!     writer_close(tiff, "test description");
 //! }
-//!
-//! // write
-//! let tiff = writer_open("./tests/test_data/cell32_mod.tif", bits, width, height).unwrap();
-//! writer_write_image_float(tiff, &buffer);
-//! writer_close(tiff, "test description");
 //! ```
 
 extern crate libc;
