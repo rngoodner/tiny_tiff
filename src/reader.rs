2419a75daf91ee9bee9dec6dfc6fd42fbb57b38e
ryn1x <ryn1x@tutanota.com>
2018-12-28 15:29:59 -0700

    add reader and writer modules

---
+++ src/reader.rs
@@ -0,0 +1,283 @@
+use libc::FILE;
+use std::ffi::c_void;
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::os::raw::c_char;
+use std::os::raw::c_double;
+use std::os::raw::c_float;
+use std::os::raw::c_int;
+use std::os::raw::c_long;
+
+const TIFF_LAST_ERROR_SIZE: usize = 1024;
+
+#[repr(C)]
+pub struct TinyTIFFReaderFrame {
+    width: u32,
+    height: u32,
+    compression: u16,
+    rowsperstrip: u32,
+    stripoffsets: *mut u32,
+    stripbytecounts: *mut u32,
+    stripcount: u32,
+    samplesperpixel: u16,
+    bitspersample: *mut u16,
+    planarconfiguration: u16,
+    sampleformat: u16,
+    imagelength: u32,
+    description: *mut c_char,
+}
+
+#[repr(C)]
+pub struct TinyTIFFReaderFile {
+    file: *mut FILE,
+    lastError: [c_char; TIFF_LAST_ERROR_SIZE],
+    wasError: c_int,
+    systembyteorder: u8,
+    filebyteorder: u8,
+    firstrecord_offset: u32,
+    nextifd_offset: u32,
+    filesize: u64,
+    currentFrame: TinyTIFFReaderFrame,
+}
+
+#[link(name = "tinytiff")]
+extern "C" {
+    fn TinyTIFFReader_open(filename: *const c_char) -> *mut TinyTIFFReaderFile;
+    fn TinyTIFFReader_close(tiff: *mut TinyTIFFReaderFile);
+    fn TinyTIFFReader_getBitsPerSample(tiff: *mut TinyTIFFReaderFile, sample: c_int) -> u16;
+    fn TinyTIFFReader_getSampleData(
+        tiff: *mut TinyTIFFReaderFile,
+        sample_data: *mut c_void,
+        sample: u16,
+    ) -> c_int;
+    fn TinyTIFFReader_getWidth(tiff: *mut TinyTIFFReaderFile) -> c_int;
+    fn TinyTIFFReader_getHeight(tiff: *mut TinyTIFFReaderFile) -> c_int;
+    fn TinyTIFFReader_countFrames(tiff: *mut TinyTIFFReaderFile) -> c_int;
+    fn TinyTIFFReader_getSampleFormat(tiff: *mut TinyTIFFReaderFile) -> u16;
+    fn TinyTIFFReader_getSamplesPerPixel(tiff: *mut TinyTIFFReaderFile) -> u16;
+    fn TinyTIFFReader_getImageDescription(tiff: *mut TinyTIFFReaderFile) -> *const c_char;
+    fn TinyTIFFReader_hasNext(tiff: *mut TinyTIFFReaderFile) -> c_int;
+    fn TinyTIFFReader_readNext(tiff: *mut TinyTIFFReaderFile) -> c_int;
+    fn TinyTIFFReader_success(tiff: *mut TinyTIFFReaderFile) -> c_int;
+    fn TinyTIFFReader_wasError(tiff: *mut TinyTIFFReaderFile) -> c_int;
+    fn TinyTIFFReader_getLastError(tiff: *mut TinyTIFFReaderFile) -> *const c_char;
+}
+
+/// open tiff file for reading
+pub fn open(filename: &str) -> Result<*mut TinyTIFFReaderFile, String> {
+    let cfilename = CString::new(filename).unwrap();
+    let pntr = cfilename.as_ptr();
+    let tiff = unsafe { TinyTIFFReader_open(pntr) };
+    match tiff.is_null() {
+        false => Ok(tiff),
+        true => Err(format!("Could not open file: {}", String::from(filename))),
+    }
+}
+
+/// close tiff file
+pub fn close(tiff: *mut TinyTIFFReaderFile) {
+    unsafe { TinyTIFFReader_close(tiff) };
+}
+
+/// get bits per sample of current frame
+pub fn bits_per_sample(tiff: *mut TinyTIFFReaderFile, sample: i32) -> u16 {
+    let bits = unsafe { TinyTIFFReader_getBitsPerSample(tiff, sample) };
+    bits
+}
+
+/// read data from current frame into supplied buffer
+pub fn sample_data<T>(tiff: *mut TinyTIFFReaderFile, buffer: &Vec<T>, sample: u16) -> bool {
+    let pntr = buffer.as_ptr() as *mut c_void;
+    let data = unsafe { TinyTIFFReader_getSampleData(tiff, pntr, sample) };
+    data != 0
+}
+
+/// get width of current frame
+pub fn width(tiff: *mut TinyTIFFReaderFile) -> u32 {
+    let width = unsafe { TinyTIFFReader_getWidth(tiff) };
+    width as u32
+}
+
+/// get height of current frame
+pub fn height(tiff: *mut TinyTIFFReaderFile) -> u32 {
+    let height = unsafe { TinyTIFFReader_getHeight(tiff) };
+    height as u32
+}
+
+/// get number of frames
+pub fn count_frames(tiff: *mut TinyTIFFReaderFile) -> i32 {
+    let frames = unsafe { TinyTIFFReader_countFrames(tiff) };
+    frames
+}
+
+/// get sample format of current frame
+pub fn sample_format(tiff: *mut TinyTIFFReaderFile) -> u16 {
+    let format = unsafe { TinyTIFFReader_getSampleFormat(tiff) };
+    format
+}
+
+/// get samples per pixel of current from
+pub fn samples_per_pixel(tiff: *mut TinyTIFFReaderFile) -> u16 {
+    let format = unsafe { TinyTIFFReader_getSamplesPerPixel(tiff) };
+    format
+}
+
+/// get image description of current frame
+pub fn image_description(tiff: *mut TinyTIFFReaderFile) -> String {
+    let desc = unsafe { TinyTIFFReader_getImageDescription(tiff) };
+    let desc = unsafe { CStr::from_ptr(desc) };
+    let desc = desc.to_str().unwrap();
+    let desc = String::from(desc);
+    desc
+}
+
+/// true if another frame exists
+pub fn has_next(tiff: *mut TinyTIFFReaderFile) -> bool {
+    let result = unsafe { TinyTIFFReader_hasNext(tiff) };
+    result != 0
+}
+
+/// read next frame from a multi-frame tiff
+pub fn read_next(tiff: *mut TinyTIFFReaderFile) -> bool {
+    let result = unsafe { TinyTIFFReader_readNext(tiff) };
+    result != 0
+}
+
+/// true if no error in last function call
+pub fn success(tiff: *mut TinyTIFFReaderFile) -> bool {
+    let result = unsafe { TinyTIFFReader_success(tiff) };
+    result != 0
+}
+
+/// true if error in last function call
+pub fn was_error(tiff: *mut TinyTIFFReaderFile) -> bool {
+    let result = unsafe { TinyTIFFReader_wasError(tiff) };
+    result != 0
+}
+
+/// get last error messsage
+pub fn last_error(tiff: *mut TinyTIFFReaderFile) -> String {
+    let error = unsafe { TinyTIFFReader_getLastError(tiff) };
+    let error = unsafe { CStr::from_ptr(error) };
+    let error = error.to_str().unwrap();
+    let error = String::from(error);
+    error
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_open() {
+        let _tiff = open("./tests/test_data/cell8.tif").unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn open_bad_file_panics() {
+        let _tiff = open("./does/not/exist.tif").unwrap();
+    }
+
+    #[test]
+    fn can_close() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        close(tiff);
+    }
+
+    #[test]
+    fn can_bits_per_sample() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(bits_per_sample(tiff, 0), 8);
+        close(tiff);
+    }
+
+    #[test]
+    fn can_sample_data() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        let width = width(tiff);
+        let height = height(tiff);
+        let size = (width * height) as usize;
+        let mut buffer: Vec<u8> = vec![0u8; size];
+        let result = sample_data(tiff, &buffer, 0);
+        close(tiff);
+        assert!(result);
+        assert_eq!(buffer[2], 112 as u8);
+    }
+
+    #[test]
+    fn can_width() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(width(tiff), 191);
+        close(tiff);
+    }
+
+    #[test]
+    fn can_height() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(height(tiff), 159);
+        close(tiff);
+    }
+
+    #[test]
+    fn can_count_frames() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(count_frames(tiff), 1);
+        close(tiff);
+    }
+
+    #[test]
+    fn can_sample_format() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(sample_format(tiff), 1);
+        close(tiff);
+    }
+
+    #[test]
+    fn can_samples_per_pixel() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(samples_per_pixel(tiff), 1);
+        close(tiff);
+    }
+
+    #[test]
+    fn can_image_description() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(image_description(tiff), "image description");
+        close(tiff);
+    }
+
+    #[test]
+    fn can_has_next() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert!(!has_next(tiff));
+        close(tiff);
+    }
+
+    #[test]
+    fn can_read_next() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert!(!read_next(tiff));
+        close(tiff);
+    }
+
+    #[test]
+    fn can_success() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert!(success(tiff));
+        close(tiff);
+    }
+
+    #[test]
+    fn can_was_error() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert!(!was_error(tiff));
+        close(tiff);
+    }
+
+    #[test]
+    fn can_last_error() {
+        let tiff = open("./tests/test_data/cell8.tif").unwrap();
+        assert_eq!(last_error(tiff), "");
+        close(tiff);
+    }
+}
---
+++ src/writer.rs
@@ -0,0 +1,159 @@
+use libc::FILE;
+use std::ffi::c_void;
+use std::ffi::CString;
+use std::os::raw::c_char;
+use std::os::raw::c_double;
+use std::os::raw::c_float;
+use std::os::raw::c_int;
+use std::os::raw::c_long;
+
+#[repr(C)]
+pub struct TinyTIFFFile {
+    file: *mut FILE,
+    lastIFDOffsetField: u32,
+    lastStartPos: c_long,
+    lastIFDDATAAdress: u32,
+    lastIFDCount: u16,
+    lastHeader: *mut u8,
+    lastHeaderSize: c_int,
+    pos: u32,
+    width: u32,
+    height: u32,
+    bitspersample: u16,
+    descriptionOffset: u32,
+    descriptionSizeOffset: u32,
+    frames: u64,
+    byteorder: u8,
+}
+
+#[link(name = "tinytiff")]
+extern "C" {
+    fn TinyTIFFWriter_open(
+        filename: *const c_char,
+        bits_per_sample: u16,
+        width: u32,
+        height: u32,
+    ) -> *mut TinyTIFFFile;
+    fn TinyTIFFWriter_getMaxDescriptionTextSize(tiff: *mut TinyTIFFFile) -> c_int;
+    fn TinyTIFFWriter_close(tiff: *mut TinyTIFFFile, image_description: *const c_char);
+    fn TinyTIFFWriter_writeImageVoid(tiff: *mut TinyTIFFFile, image_data: *mut c_void);
+    fn TinyTIFFWriter_writeImageFloat(tiff: *mut TinyTIFFFile, image_data: *mut c_float);
+    fn TinyTIFFWriter_writeImageDouble(tiff: *mut TinyTIFFFile, image_data: *mut c_double);
+}
+
+/// create a new tiff file
+pub fn open(
+    filename: &str,
+    bits_per_sample: u16,
+    width: u32,
+    height: u32,
+) -> Result<*mut TinyTIFFFile, String> {
+    let cfilename = CString::new(filename).unwrap();
+    let pntr = cfilename.as_ptr();
+    let tiff = unsafe { TinyTIFFWriter_open(pntr, bits_per_sample, width, height) };
+    match tiff.is_null() {
+        false => Ok(tiff),
+        true => Err(format!("Could not open file: {}", String::from(filename))),
+    }
+}
+
+/// get max size for image description
+pub fn max_description_text_size(tiff: *mut TinyTIFFFile) -> i32 {
+    let size = unsafe { TinyTIFFWriter_getMaxDescriptionTextSize(tiff) };
+    size
+}
+
+/// close the tiff and write image description to first frame
+pub fn close(tiff: *mut TinyTIFFFile, image_description: &str) {
+    let image_description = CString::new(image_description).unwrap();
+    let image_description = image_description.as_ptr();
+    unsafe { TinyTIFFWriter_close(tiff, image_description) };
+}
+
+/// writes row-major image data to a tiff file
+pub fn write_image_void<T>(tiff: *mut TinyTIFFFile, buffer: &Vec<T>) {
+    let pntr = buffer.as_ptr() as *mut c_void;
+    unsafe { TinyTIFFWriter_writeImageVoid(tiff, pntr) };
+}
+
+/// writes row-major image data to a tiff file
+pub fn write_image_float<T>(tiff: *mut TinyTIFFFile, buffer: &Vec<T>) {
+    let pntr = buffer.as_ptr() as *mut c_float;
+    unsafe { TinyTIFFWriter_writeImageFloat(tiff, pntr) };
+}
+
+/// writes row-major image data to a tiff file
+pub fn write_image_double<T>(tiff: *mut TinyTIFFFile, buffer: &Vec<T>) {
+    let pntr = buffer.as_ptr() as *mut c_double;
+    unsafe { TinyTIFFWriter_writeImageDouble(tiff, pntr) };
+}
+
+mod tests {
+    use super::*;
+
+    //#[test]
+    //fn can_open() {
+    //let _tiff = open("./tests/test_data/cell2.tif", 8, 100, 100).unwrap();
+    //}
+
+    #[test]
+    #[should_panic]
+    fn open_bad_file_panics() {
+        let _tiff = open("./does/not/exist.tif", 8, 100, 100).unwrap();
+    }
+
+    //#[test]
+    //fn can_max_description_text_size() {
+    //let tiff = open("./tests/test_data/cell2.tif", 8, 100, 100).unwrap();
+    //let size = max_description_text_size(tiff);
+    //assert_ne!(size, 0);
+    //}
+
+    #[test]
+    fn can_write_image_void8_and_close() {
+        let bits = 8;
+        let width: u32 = 10;
+        let height: u32 = 100;
+        let size = (width * height) as usize;
+        let mut buffer: Vec<u8> = vec![42u8; size];
+        let tiff = open("./tests/test_data/test8.tif", bits, width, height).unwrap();
+        write_image_void(tiff, &buffer);
+        close(tiff, "test 8bit");
+    }
+
+    #[test]
+    fn can_write_image_void16_and_close() {
+        let bits = 16;
+        let width: u32 = 10;
+        let height: u32 = 100;
+        let size = (width * height) as usize;
+        let mut buffer: Vec<u16> = vec![42u16; size];
+        let tiff = open("./tests/test_data/test16.tif", bits, width, height).unwrap();
+        write_image_void(tiff, &buffer);
+        close(tiff, "test 16bit");
+    }
+
+    #[test]
+    fn can_write_image_float32_and_close() {
+        let bits = 32;
+        let width: u32 = 10;
+        let height: u32 = 100;
+        let size = (width * height) as usize;
+        let mut buffer: Vec<f32> = vec![42f32; size];
+        let tiff = open("./tests/test_data/test32.tif", bits, width, height).unwrap();
+        write_image_float(tiff, &buffer);
+        close(tiff, "test 32bit");
+    }
+
+    #[test]
+    fn can_write_image_double64_and_close() {
+        let bits = 64;
+        let width: u32 = 10;
+        let height: u32 = 100;
+        let size = (width * height) as usize;
+        let mut buffer: Vec<f64> = vec![42f64; size];
+        let tiff = open("./tests/test_data/test64.tif", bits, width, height).unwrap();
+        write_image_double(tiff, &buffer);
+        close(tiff, "test 64bit");
+    }
+}
