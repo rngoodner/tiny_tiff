63d2239b8fc2abd292556b5b011653e8c4b4aab7
ryn1x <ryn1x@tutanota.com>
2018-12-28 23:31:40 -0700

    fixed doc test

--- src/lib.rs
+++ src/lib.rs
@@ -36,27 +36,25 @@
 //! use tiny_tiff::reader;
 //! use tiny_tiff::writer;
 //!
-//! fn manipulate_image() {
-//!     // read
-//!     let tiff = reader::open("./tests/test_data/cell32.tif").unwrap();
-//!     let bits = reader::bits_per_sample(tiff, 0);
-//!     let width = reader::width(tiff);
-//!     let height = reader::height(tiff);
-//!     let size = (width * height) as usize;
-//!     let mut buffer: Vec<f32> = vec![0f32; size];
-//!     reader::sample_data(tiff, &buffer, 0);
-//!     reader::close(tiff);
+//! // read
+//! let tiff = reader::open("./tests/test_data/cell32.tif").unwrap();
+//! let bits = reader::bits_per_sample(tiff, 0);
+//! let width = reader::width(tiff);
+//! let height = reader::height(tiff);
+//! let size = (width * height) as usize;
+//! let mut buffer: Vec<f32> = vec![0f32; size];
+//! reader::sample_data(tiff, &buffer, 0);
+//! reader::close(tiff);
 //!
-//!     // manipulate
-//!     for px in &mut buffer {
-//!         *px += 42f32;
-//!     }
-//!
-//!     // write
-//!     let tiff = writer::open("./tests/test_data/cell32_example.tif", bits, width, height).unwrap();
-//!     writer::write_image_float(tiff, &buffer);
-//!     writer::close(tiff, "test description");
+//! // manipulate
+//! for px in &mut buffer {
+//!     *px += 42f32;
 //! }
+//!
+//! // write
+//! let tiff = writer::open("./tests/test_data/cell32_example.tif", bits, width, height).unwrap();
+//! writer::write_image_float(tiff, &buffer);
+//! writer::close(tiff, "cell32 + 42!");
 //! ```
 
 extern crate libc;
