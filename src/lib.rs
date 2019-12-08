a41d55ee71f45b9dd0a40ed05e000a7fd2a1ff18
ryn1x <ryangoodner@protonmail.com>
2019-12-08 00:24:52 -0700

    updates documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,3 @@
-//! # tiny_tiff
-//!
-//! `tiny_tiff` is a wrapper for the TinyTIFF C++ library. It enables easy reading and writing of
-//! uncompressed TIFF images with uint, int, or float data types.
-//!
-//! DEPENDANCIES
-//! ============
-//!
-//! ## Unix
-//!
-//! - `git clone https://github.com/ryn1x/TinyTIFF.git`
-//! - `cd TinyTIFF`
-//! - `mkdir build`
-//! - `cd build`
-//! - `cmake ..`
-//! - `make`
-//! - `sudo make install`
-//!
-//!
-//! ## Windows
-//!
-//! - `git clone https://github.com/ryn1x/TinyTIFF.git`
-//! - `cd TinyTIFF`
-//! - `mkdir build`
-//! - `cd build`
-//! - `cmake ..`
-//! - `cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DBUILD_SHARED_LIBS=TRUE -G "Visual Studio 15 2017 win64" ..`
-//! - build generated ".sln" file with visual studio
-//!
 //! SYNOPSIS
 //! ========
 //!
